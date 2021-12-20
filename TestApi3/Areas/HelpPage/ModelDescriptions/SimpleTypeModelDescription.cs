607c58938ae6038b4af07ef0db979492a3714f77
FTI <FTI@NOTE-MATHEUSRES>
2021-12-20 16:50:52 -0300

    Add project files.

---
+++ TestApi3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestApi3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TestApi3/Controllers/ProdutosController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestApi3.Models;
+
+namespace TestApi3.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        Entities db = new Entities();
+        
+        //Post
+        public string Post(Produto produto)
+        {
+            db.Produtos.Add(produto);
+            db.SaveChanges();
+            return "Número de clientes adicionado com sucesso";
+        } 
+        //Get
+        public IEnumerable <Produto> Get()
+        {
+            return db.Produtos.ToList();
+        }
+
+        //Update
+        public string Put (int id, Produto protudo)
+        {
+            var encontrar_produto = db.Produtos.Find(id);
+            encontrar_produto.Cliente = protudo.Cliente;
+            encontrar_produto.Quantidade = protudo.Quantidade;
+            db.Entry(encontrar_produto).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return "Atualizado com sucesso";
+        }
+
+
+        //Delete
+        public string Delete(int id)
+        {
+            Produto produto = db.Produtos.Find(id);
+            db.Produtos.Remove(produto);
+            db.SaveChanges();
+            return "Deletado com sucesso";
+        }
+
+    }
+}
